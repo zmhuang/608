82d07d0a0047cc38a24b1d4ccd6f5c3906305001
huangzhimin <zm-huang@126.com>
2015-06-14 22:07:44 +0800

    x

---
+++ conf/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+
+. ~/608/config
+cd ~/install-cassandra/dsc-cassandra-2.1.6
+bin/cassandra
--- conf/up.sh
+++ conf/up.sh
@@ -3,3 +3,4 @@
 set -e
 sudo /bin/cp limits.conf /etc/security/limits.conf
 /bin/cp bash_profile ~/.bash_profile
+/bin/cp cassandra.yaml ~/install-cassandra/dsc-cassandra-2.1.6/conf
