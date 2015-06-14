0bed570b5f4755260885ab0ee18808467b792d83
huangzhimin <zm-huang@126.com>
2015-06-14 23:34:02 +0800

    x

--- conf/install.sh
+++ conf/install.sh
@@ -8,17 +8,11 @@ WGET="wget -c --timeout=15"
 mkdir -p $INSTALL_DIR
 cd $INSTALL_DIR
 
-if [ ! -f kairosdb-1.0.0-1.tar.gz ] ; then
-    $WGET https://github.com/kairosdb/kairosdb/releases/download/v1.0.0/kairosdb-1.0.0-1.tar.gz
-    tar xf kairosdb-1.0.0-1.tar.gz
-fi
+$WGET https://github.com/kairosdb/kairosdb/releases/download/v1.0.0/kairosdb-1.0.0-1.tar.gz
+tar xf kairosdb-1.0.0-1.tar.gz
 
-if [ ! -f jna-4.1.0.jar ] ; then
-    $WGET https://maven.java.net/content/repositories/releases/net/java/dev/jna/jna/4.1.0/jna-4.1.0.jar
-    tar xf dsc-cassandra-2.1.6-bin.tar.gz
-fi
+$WGET http://downloads.datastax.com/community/dsc-cassandra-2.1.6-bin.tar.gz
+tar xf dsc-cassandra-2.1.6-bin.tar.gz
 
-if [ ! -f dsc-cassandra-2.1.6-bin.tar.gz ] ; then
-    $WGET http://downloads.datastax.com/community/dsc-cassandra-2.1.6-bin.tar.gz
-    /bin/cp jna-4.1.0.jar dsc-cassandra-2.1.6/lib
-fi
+$WGET https://maven.java.net/content/repositories/releases/net/java/dev/jna/jna/4.1.0/jna-4.1.0.jar
+/bin/cp jna-4.1.0.jar dsc-cassandra-2.1.6/lib
