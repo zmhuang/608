ac2ae02b08c63564d2ecce87b260e2710a4e3393
huangzhimin <zm-huang@126.com>
2015-06-15 00:04:13 +0800

    x

--- conf/up.sh
+++ conf/up.sh
@@ -5,3 +5,4 @@ set -e
 sudo $myCP limits.conf /etc/security/limits.conf
 $myCP bash_profile $HOME/.bash_profile
 $myCP cassandra.yaml $INSTALL_DIR/dsc-cassandra-2.1.6/conf
+$myCP kairosdb.properties $INSTALL_DIR/kairosdb/conf/kairosdb.properties
