0fa9e6d28916373fc8bac73aa2a72668cad44e2f
huangzhimin <zm-huang@126.com>
2015-06-22 23:26:32 +0800

    x

--- client/demo.py
+++ client/demo.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# 关闭，退出
 #
 
 import time
@@ -14,15 +13,20 @@ metric_name = 'deformation'
 metrics = []
 
 # 提交测量值 deformation/变形
-for i in range(1):
-    print 'i=',i
-    conn.write_one_metric(metric_name, time.time(), 1.2 + i, {'bridgeID':1, 'pointID':1})
-    #time.sleep(1.0)
-    i += 1
+conn.write_one_metric(metric_name, time.time(), 11.2, {'bridgeID':1, 'pointID':1})
 
 # 查询过去 24 小时产生的数据
 result=conn.read_relative([metric_name], start_time=(1, 'days'))
 print result
 print "sample_size=",result['queries'][0]['sample_size']
 print "samples=",result['queries'][0]['results'][0]['values']
-#print "samples_size=",len(result['queries'][0]['results'][0]['values'])
+for i in result['queries'][0]['results'][0]['values']:
+    print i[0],',',i[1]
+# 测量值大于 5.0 的
+for i in result['queries'][0]['results'][0]['values']:
+    if i[1] > 5.0:
+        print i
+
+# 关闭，退出
+result = None
+conn = None
